5f90f3ce8f386479eb50b8e3009045fa5000b3eb
AndoniOka <91051757+AndoniOka@users.noreply.github.com>
2024-04-06 20:50:30 +0200

    User model, controller, repository and service

---
+++ BSPQ23-E4/LuxWheels/server/src/main/java/com/example/server/UserController.java
@@ -0,0 +1,39 @@
+package com.example.server;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Integer id) {
+        return userService.getUserById(id);
+    }
+
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable Integer id, @RequestBody User user) {
+        return userService.updateUser(id, user);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Integer id) {
+        userService.deleteUser(id);
+    }
+}
---
+++ BSPQ23-E4/LuxWheels/server/src/main/java/com/example/server/UserRepository.java
@@ -0,0 +1,12 @@
+package com.example.server;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer>{
+
+	Optional<User> findById(Integer id);
+
+	void deleteById(Integer id);
+
+}
