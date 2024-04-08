21c90dec924a9cbcf5fd833cd58bde6d844bf512
Ander Perez-Caballero Unzain <100701598+AnderPerezCaballero@users.noreply.github.com>
2024-04-08 17:15:00 +0200

    CarRepository and CarService

---
+++ LuxWheels/server/src/main/java/com/example/server/repository/CarRepository.java
@@ -0,0 +1,12 @@
+package com.example.server.repository;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.server.entity.Car;
+
+public interface CarRepository extends JpaRepository<Car, Integer>{
+
+	Optional<Car> findByLicensePlate(Integer lp);
+
+	void deleteByLicensePlate(Integer lp);
+
+}
---
+++ LuxWheels/server/src/main/java/com/example/server/service/CarService.java
@@ -0,0 +1,34 @@
+package com.example.server.service;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.example.server.entity.Car;
+import com.example.server.repository.CarRepository;
+
+@Service
+public class CarService {
+    @Autowired
+    private CarRepository carRepository;
+
+    public Car createCar(Car car) {
+        return carRepository.save(car);
+    }
+
+    public List<Car> getAllCars() {
+        return carRepository.findAll();
+    }
+
+    public Car getUserByLicensePlate(Integer lp) {
+        return carRepository.findByLicensePlate(lp).orElse(null);
+    }
+
+    public Car updateCar(Integer lp, Car car) {
+        car.setLicensePlate(lp); //I think this fails due to the lack of getters and setters in Car class
+        return carRepository.save(car);
+    }
+
+    public void deleteUser(Integer lp) {
+        carRepository.deleteByLicensePlate(lp);
+    }
+}
