90b3a624ece178a452aa0a5d8d11a3fc167af0b2
Ander Perez-Caballero Unzain <100701598+AnderPerezCaballero@users.noreply.github.com>
2024-04-08 17:44:44 +0200

    CarRepository and CarService

--- LuxWheels/server/src/main/java/com/example/server/repository/CarRepository.java
+++ LuxWheels/server/src/main/java/com/example/server/repository/CarRepository.java
@@ -3,6 +3,7 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.server.entity.Car;
 
+
 public interface CarRepository extends JpaRepository<Car, Integer>{
 
 	Optional<Car> findByLicensePlate(Integer lp);
--- LuxWheels/server/src/main/java/com/example/server/service/CarService.java
+++ LuxWheels/server/src/main/java/com/example/server/service/CarService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import com.example.server.entity.Car;
 import com.example.server.repository.CarRepository;
 
+
 @Service
 public class CarService {
     @Autowired
